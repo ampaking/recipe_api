7d6e8dce4879eb6aa1ff3d0108a47938faffc8cb
ampaking <78628177+ampaking@users.noreply.github.com>
2022-11-13 13:22:42 +0900

    mend
    add email validation & normalize email with tests

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,40 @@
+"""
+Test for models.
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModalTests(TestCase):
+    """Test User model"""
+
+    def test_create_user_with_email_successful(self):
+        """Test user to create with email."""
+        email = 'test@example.com'
+        password = 'testPass'
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalize(self):
+        """Test email is normalized for new users."""
+
+        sample_emails = [['test1@EXAMPLE.com', 'test1@example.com'],
+                         ['Test2@Example.com', 'Test2@example.com'],
+                         ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+                         ['test4@example.COM', 'test4@example.com']]
+        for email, excepted in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, excepted)
+
+    def test_new_user_with_out_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test212')
